9e626e166a3d8cad51bb741b810f9d767c53b74a
Guest <guest-w0JUGT@p215r1k06.edu.vkhk.ee>
2016-03-24 12:18:30 +0200

    index.php

--- index.php
+++ index.php
@@ -3,7 +3,20 @@
 <?php
 $get = getUserList();
 setlocale(LC_TIME, 'Estonia');
+date_default_timezone_set('Europe/Tallinn');
+$algus = //Our dates
+$date1 = "2016-03-01 10:00:00";
+$date2 = date("Y-m-d H:i:s");
+ 
+//Convert them to timestamps.
+$date1Timestamp = strtotime($date1);
+$date2Timestamp = strtotime($date2);
+ 
+//Calculate the difference.
+$difference = $date2Timestamp - $date1Timestamp;
+
 echo "Tänane kuupäev: ".(strftime("%Y. %B %d."));
+echo "Kursuse algusest on möödas: ".$difference." sek.";
 ?>
 <div class="page-header pool">
         <h1>Tabel</h1>
